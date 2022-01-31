fd9c216664f78204e97777c5050b0e21f15f5c78
Bruno Alves <hello@brunoalves.app>
2022-01-31 17:24:42 +0000

    Header Component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+  const [isUserLoggedIn] = useState(true)
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      John Doe
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign In with Github
+    </button>
+  )
+}
