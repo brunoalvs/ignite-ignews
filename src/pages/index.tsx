d270c9fbff42ef65f0f10ca3af8a0cf40ab19a88
Bruno Alves <hello@brunoalves.app>
2022-01-31 10:23:56 +0000

    Estilos globais na aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Página inicial da ig.news" />
+      </Head>
+
+      <h1>Hello world</h1>
+    </>
+  )
 }
