7e9d9156ebc4353229735ded97f1f9951a663666
Bruno Alves <hello@brunoalves.app>
2022-01-31 17:39:37 +0000

    Do jeitinho que o Diego fez

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <meta name="description" content="Página inicial da ig.news" />
+        <meta name="description" content="Home page | ig.news" />
       </Head>
 
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $10 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
