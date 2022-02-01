775f5da7eb3c2f47e7337a00a902db595504f214
Bruno Alves <hello@brunoalves.app>
2022-02-01 11:30:29 +0000

    SSR applied

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,12 +27,34 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $10 month</span>
+            <span>
+              for{' '}
+              {new Intl.NumberFormat('en', {
+                style: 'currency',
+                currency: 'EUR',
+              }).format(product.amount)}{' '}
+              month
+            </span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KO5SwAm3jniLxS4CtIIknYk')
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  }
+
+  return {
+    props: {
+      product,
+    },
+  }
 }
